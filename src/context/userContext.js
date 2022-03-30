00caca526c2c812854dccf6e65e2aa28dc84709d
Himanshu <mailboxofhimanshu1999@gmail.com>
2022-03-31 00:39:26 +0530

    Update userContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,16 +1,36 @@
-import React, { createContext } from "react";
+// import React, { createContext } from "react";
 
 
-const UserContext = createContext()
+// const UserContext = createContext()
+
+// const Wrapper = (props) => {
+
+//     const greeting = "Hello"
+  
+//     return (
+//         <UserContext.Provider value={{ greeting }}>
+//             {props.children}
+//         </UserContext.Provider>
+//     )
+// }
+// export { Wrapper, UserContext }
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
 
 const Wrapper = (props) => {
+  const [greeting, setGreeting] = useState("Hello");
+  //const greeting = state;
+  //let greeting = "pass11";
+  // const changeGreeting = (e) => {
+  //   console.log("greeting");
+  //   //setGreeting(e);
+  // };
 
-    const greeting = "Hello"
-  
-    return (
-        <UserContext.Provider value={{ greeting }}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
-export { Wrapper, UserContext }
+  return (
+    <UserContext.Provider value={{ greeting, setGreeting }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+export { Wrapper, UserContext };
